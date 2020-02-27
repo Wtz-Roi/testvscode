2509ef76606cbd52c263da1045a4f0255fa13ecd
WTang Tongzhou <18700416150@163.com>
2020-02-27 18:12:18 +0800

    第四次提交

--- cod2.cpp
+++ cod2.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
     int a=10;
-    
+    string s="tongzhou";
     cout<<a<<endl;
     cout<<"hello,world"<<endl;
-
+    cout<<s<<endl;
     return 0;
 }
